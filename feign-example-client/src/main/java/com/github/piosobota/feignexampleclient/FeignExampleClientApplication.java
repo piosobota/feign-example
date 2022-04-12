69dfd67873c3e363527525c99f60e9c89c78a01e
piosobota <piotrsobota32@gmail.com>
2022-04-12 12:37:07 +0200

    Added client and server

---
+++ feign-example-client/src/main/java/com/github/piosobota/feignexampleclient/FeignExampleClientApplication.java
@@ -0,0 +1,13 @@
+package com.github.piosobota.feignexampleclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FeignExampleClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FeignExampleClientApplication.class, args);
+	}
+
+}
---
+++ feign-example-client/src/test/java/com/github/piosobota/feignexampleclient/FeignExampleClientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.piosobota.feignexampleclient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FeignExampleClientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
