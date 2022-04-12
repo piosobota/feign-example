de61292d9bbbf6e32a5258f878f318796aef7ec6
piosobota <piotrsobota32@gmail.com>
2022-04-12 11:43:01 +0200

    Added spring boot starter files

---
+++ src/main/java/com/github/piosobota/feignexample/FeignExampleApplication.java
@@ -0,0 +1,13 @@
+package com.github.piosobota.feignexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FeignExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FeignExampleApplication.class, args);
+	}
+
+}
